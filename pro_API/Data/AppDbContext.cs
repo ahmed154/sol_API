7929bdb297d11e9f5fbfac1c655452a1ccf56e58
ahmed1542010 <ahmed1542010@gmail.com>
2020-07-01 20:20:25 +0300

    Add project files.

---
+++ pro_API/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pro_API.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
